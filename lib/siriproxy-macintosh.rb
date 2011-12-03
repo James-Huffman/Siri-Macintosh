1eb1a0e874a594bf493c90ecf1b00146d19cd2bd
Loris1634 <Loris1634@gmail.com>
2011-12-03 19:11:05 +0100

    Added battery informations

--- lib/siriproxy-macintosh.rb
+++ lib/siriproxy-macintosh.rb
@@ -27,11 +27,11 @@ class SiriProxy::Plugin::Macintosh < SiriProxy::Plugin
    listen_for(/mac.*pause/i) { itunes_pause }
     listen_for(/mac.*play/i) { itunes_play }
 
+#battery
+listen_for(/mac.*batterie/i) { set_battery_status }
   
-  #Applications
-  
-
-
+  #Macintosh
+ 
 	def set_mac_sleep
 	say "Mise en veille de votre Mac"
 
@@ -58,6 +58,10 @@ class SiriProxy::Plugin::Macintosh < SiriProxy::Plugin
 	system("osascript -e 'tell application \"iTunes\" to quit'");
 	end
 
+
+    def play_random_song
+    
+    end
     def itunes_next
     
     system("osascript -e 'tell application \"iTunes\" to next track'");
@@ -79,6 +83,57 @@ class SiriProxy::Plugin::Macintosh < SiriProxy::Plugin
     
     end
     
-####### END ###
-    
+
+
+
+def get_file_as_string(filename)
+  data = ''
+  f = File.open(filename, "r") 
+  f.each_line do |line|
+    data += line
+  end
+  return data
+end
+
+def set_battery_status 
+
+
+## BATTERY ##
+ system("ioreg -l | grep Capacity > ~/Desktop/test.txt");
+
+fileBattery = get_file_as_string '/Users/loris1634/Desktop/test.txt'
+fileBattery2MAX = fileBattery.scan(/\d+/)[0]
+fileBattery2Current = fileBattery.scan(/\d+/)[1]
+
+p1 = fileBattery2Current.to_i
+p2 = fileBattery2MAX.to_i
+pourcentageBATTERY = (p1.to_f / p2.to_f) * 100
+
+say "Il reste #{pourcentageBATTERY} pourcents de batterie sur votre Mac."
+
+###
+
+### POWERED ###
+
+system("ioreg -w0 -l | grep \"ExternalConnected\" > ~/Desktop/test2.txt");
+
+filePowered = get_file_as_string '/Users/loris1634/Desktop/test2.txt'
+
+if filePowered.include? "No"
+say "Votre Mac n'est pas branche au secteur"
+if(pourcentageBATTERY < 21.000000000)
+
+say "Vous devriez brancher votre ordinateur, il reste moins de 20% de batterie."
+end
+else
+say "Votre Mac est en train de recharger"
+
+
+end
+
+###
+
+end  
+
+  
 end
